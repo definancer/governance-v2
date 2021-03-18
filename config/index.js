b6aed4ccce58d9348d1333933ea5b083acd93012
definancer <txdev6@gmail.com>
2021-03-18 14:32:58 +0800

    del: truffle

--- config/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  network: 'ganache',
-  ganache: require('./ganache'),
-  geth: require('./geth'),
-  deploy: {
-    gas: 800e4, //gaslimit
-    gasPrice: 20e9, //20 Wei
-  },
-};
